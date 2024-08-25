41788275b326127c3cf09a6f9bb090ef3b441e26
mohdshoaibkhan <mohdshoaibkhan7211@gmail.com>
2024-08-25 15:19:53 +0530

    sending message

--- matter.js
+++ matter.js
@@ -1,12 +1,12 @@
-var canvas = $('#wrapper-canvas').get(0)
+var canvas = $("#wrapper-canvas").get(0);
 
 var dimensions = {
   width: $(window).width(),
   height: $(window).height(),
-}
+};
 
-Matter.use('matter-attractors')
-Matter.use('matter-wrap')
+Matter.use("matter-attractors");
+Matter.use("matter-wrap");
 
 function runMatter() {
   // module aliases
@@ -20,14 +20,14 @@ function runMatter() {
     Common = Matter.Common,
     Composite = Matter.Composite,
     Composites = Matter.Composites,
-    Bodies = Matter.Bodies
+    Bodies = Matter.Bodies;
 
   // create engine
-  var engine = Engine.create()
+  var engine = Engine.create();
 
-  engine.world.gravity.y = 0
-  engine.world.gravity.x = 0
-  engine.world.gravity.scale = 0.1
+  engine.world.gravity.y = 0;
+  engine.world.gravity.x = 0;
+  engine.world.gravity.scale = 0.1;
 
   // create renderer
   var render = Render.create({
@@ -38,19 +38,19 @@ function runMatter() {
       width: dimensions.width,
       height: dimensions.height,
       wireframes: false,
-      background: 'transparent',
+      background: "transparent",
     },
-  })
+  });
 
   // create runner
-  var runner = Runner.create()
+  var runner = Runner.create();
 
   // Runner.run(runner, engine);
   // Render.run(render);
 
   // create demo scene
-  var world = engine.world
-  world.gravity.scale = 0
+  var world = engine.world;
+  world.gravity.scale = 0;
 
   // create a body with an attractor
   var attractiveBody = Bodies.circle(
@@ -71,21 +71,22 @@ function runMatter() {
             return {
               x: (bodyA.position.x - bodyB.position.x) * 1e-6,
               y: (bodyA.position.y - bodyB.position.y) * 1e-6,
-            }
+            };
           },
         ],
       },
     }
-  )
+  );
 
-  World.add(world, attractiveBody)
+  World.add(world, attractiveBody);
 
   // add some bodies that to be attracted
   for (var i = 0; i < 60; i += 1) {
-    let x = Common.random(0, render.options.width)
-    let y = Common.random(0, render.options.height)
-    let s = Common.random() > 0.6 ? Common.random(10, 80) : Common.random(4, 60)
-    let poligonNumber = Common.random(3, 6)
+    let x = Common.random(0, render.options.width);
+    let y = Common.random(0, render.options.height);
+    let s =
+      Common.random() > 0.6 ? Common.random(10, 80) : Common.random(4, 60);
+    let poligonNumber = Common.random(3, 6);
     var body = Bodies.polygon(
       x,
       y,
@@ -98,16 +99,16 @@ function runMatter() {
         frictionAir: 0.02,
         angle: Math.round(Math.random() * 360),
         render: {
-          fillStyle: '#222222',
+          fillStyle: "#222222",
           strokeStyle: `#000000`,
           lineWidth: 2,
         },
       }
-    )
+    );
 
-    World.add(world, body)
+    World.add(world, body);
 
-    let r = Common.random(0, 1)
+    let r = Common.random(0, 1);
     var circle = Bodies.circle(x, y, Common.random(2, 8), {
       mass: 0.1,
       friction: 0,
@@ -117,9 +118,9 @@ function runMatter() {
         strokeStyle: `#000000`,
         lineWidth: 2,
       },
-    })
+    });
 
-    World.add(world, circle)
+    World.add(world, circle);
 
     var circle = Bodies.circle(x, y, Common.random(2, 20), {
       mass: 6,
@@ -130,9 +131,9 @@ function runMatter() {
         strokeStyle: `#111111`,
         lineWidth: 4,
       },
-    })
+    });
 
-    World.add(world, circle)
+    World.add(world, circle);
 
     var circle = Bodies.circle(x, y, Common.random(2, 30), {
       mass: 0.2,
@@ -143,22 +144,22 @@ function runMatter() {
         strokeStyle: `#111111`,
         lineWidth: 3,
       },
-    })
+    });
 
-    World.add(world, circle)
+    World.add(world, circle);
   }
 
   // add mouse control
-  var mouse = Mouse.create(render.canvas)
+  var mouse = Mouse.create(render.canvas);
 
-  Events.on(engine, 'afterUpdate', function () {
-    if (!mouse.position.x) return
+  Events.on(engine, "afterUpdate", function () {
+    if (!mouse.position.x) return;
     // smoothly move the attractor body towards the mouse
     Body.translate(attractiveBody, {
       x: (mouse.position.x - attractiveBody.position.x) * 0.12,
       y: (mouse.position.y - attractiveBody.position.y) * 0.12,
-    })
-  })
+    });
+  });
 
   // return a context for MatterDemo to control
   let data = {
@@ -167,48 +168,46 @@ function runMatter() {
     render: render,
     canvas: render.canvas,
     stop: function () {
-      Matter.Render.stop(render)
-      Matter.Runner.stop(runner)
+      Matter.Render.stop(render);
+      Matter.Runner.stop(runner);
     },
     play: function () {
-      Matter.Runner.run(runner, engine)
-      Matter.Render.run(render)
+      Matter.Runner.run(runner, engine);
+      Matter.Render.run(render);
     },
-  }
+  };
 
-  Matter.Runner.run(runner, engine)
-  Matter.Render.run(render)
-  return data
+  Matter.Runner.run(runner, engine);
+  Matter.Render.run(render);
+  return data;
 }
 
 function debounce(func, wait, immediate) {
-  var timeout
+  var timeout;
   return function () {
     var context = this,
-      args = arguments
+      args = arguments;
     var later = function () {
-      timeout = null
-      if (!immediate) func.apply(context, args)
-    }
-    var callNow = immediate && !timeout
-    clearTimeout(timeout)
-    timeout = setTimeout(later, wait)
-    if (callNow) func.apply(context, args)
-  }
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 }
 
 function setWindowSize() {
-  let dimensions = {}
-  dimensions.width = $(window).width()
-  dimensions.height = $(window).height()
+  let dimensions = {};
+  dimensions.width = $(window).width();
+  dimensions.height = $(window).height();
 
-  m.render.canvas.width = $(window).width()
-  m.render.canvas.height = $(window).height()
-  return dimensions
+  m.render.canvas.width = $(window).width();
+  m.render.canvas.height = $(window).height();
+  return dimensions;
 }
 
-let m = runMatter()
-setWindowSize()
-$(window).resize(debounce(setWindowSize, 250))
-
-
+let m = runMatter();
+setWindowSize();
+$(window).resize(debounce(setWindowSize, 250));
--- script.js
+++ script.js
@@ -1,79 +1,72 @@
-const aboutSection = document.querySelector('.about-section')
-const aboutMeBtn = document.querySelector('.about-me-btn')
-const xIcon = document.querySelector('.x-icon')
-const navbar = document.querySelector('.navbar')
-const socialIcons = document.querySelector('.social-links')
-const overlay = document.querySelector('.overlay')
-const bodyContainer = document.querySelector('#body-container')
-const latestWorks = document.querySelector('.latest-works-link')
-const downArrow = document.querySelector('.down-arrow')
-const latestWorksArrow = document.querySelector('.latest-works-arrow')
-const blackLogo = document.querySelector('.black-logo')
+const aboutSection = document.querySelector(".about-section");
+const aboutMeBtn = document.querySelector(".about-me-btn");
+const xIcon = document.querySelector(".x-icon");
+const navbar = document.querySelector(".navbar");
+const socialIcons = document.querySelector(".social-links");
+const overlay = document.querySelector(".overlay");
+const bodyContainer = document.querySelector("#body-container");
+const latestWorks = document.querySelector(".latest-works-link");
+const downArrow = document.querySelector(".down-arrow");
+const latestWorksArrow = document.querySelector(".latest-works-arrow");
+const blackLogo = document.querySelector(".black-logo");
 
-aboutMeBtn.addEventListener('click', () => {
-  aboutSection.classList.add('active')
-  overlay.classList.add('active')
-  navbar.classList.add('hidden')
-  navbar.classList.add('hidden')
-  socialIcons.classList.add('hidden')
-})
+aboutMeBtn.addEventListener("click", () => {
+  aboutSection.classList.add("active");
+  overlay.classList.add("active");
+  navbar.classList.add("hidden");
+  navbar.classList.add("hidden");
+  socialIcons.classList.add("hidden");
+});
 
-xIcon.addEventListener('click', () => {
-  aboutSection.classList.remove('active')
-  overlay.classList.remove('active')
-  navbar.classList.remove('hidden')
-  socialIcons.classList.remove('hidden')
-})
+xIcon.addEventListener("click", () => {
+  aboutSection.classList.remove("active");
+  overlay.classList.remove("active");
+  navbar.classList.remove("hidden");
+  socialIcons.classList.remove("hidden");
+});
 
-overlay.addEventListener('click', () => {
-  aboutSection.classList.remove('active')
-  overlay.classList.remove('active')
-  navbar.classList.remove('hidden')
-  socialIcons.classList.remove('hidden')
-})
+overlay.addEventListener("click", () => {
+  aboutSection.classList.remove("active");
+  overlay.classList.remove("active");
+  navbar.classList.remove("hidden");
+  socialIcons.classList.remove("hidden");
+});
 
-canvas.addEventListener('wheel', (e) => {
-  window.scrollTo(0, (window.innerHeight * e.deltaY) / Math.abs(e.deltaY) + 5)
-})
+canvas.addEventListener("wheel", (e) => {
+  window.scrollTo(0, (window.innerHeight * e.deltaY) / Math.abs(e.deltaY) + 5);
+});
 
-latestWorks.addEventListener('click', (e) => {
-  scrollTo(0, window.innerHeight + 5)
-})
+latestWorks.addEventListener("click", (e) => {
+  scrollTo(0, window.innerHeight + 5);
+});
 
-downArrow.addEventListener('click', (e) => {
-  scrollTo(0, window.innerHeight + 5)
-})
+downArrow.addEventListener("click", (e) => {
+  scrollTo(0, window.innerHeight + 5);
+});
 
-latestWorksArrow.addEventListener('click', (e) => {
-  scrollTo(0, window.innerHeight + 5)
-})
+latestWorksArrow.addEventListener("click", (e) => {
+  scrollTo(0, window.innerHeight + 5);
+});
 
-blackLogo.addEventListener('click', (e) => {
-  scrollTo(0, 0)
-})
+blackLogo.addEventListener("click", (e) => {
+  scrollTo(0, 0);
+});
 
-// window.scrollTo(0,document.body.scrollHeight);
-
-// new GitHubCalendar('.calendar', 'anuragsinghbam', {
-//   responsive: true,
-//   tooltips: true,
-// })
-
-var swiper = new Swiper('.mySwiper', {
+var swiper = new Swiper(".mySwiper", {
   loop: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
 
-  effect: 'coverflow',
+  effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
@@ -81,4 +74,4 @@ var swiper = new Swiper('.mySwiper', {
     modifier: 1,
     slideShadows: false,
   },
-})
+});
